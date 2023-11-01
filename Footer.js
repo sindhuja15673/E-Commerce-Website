5f17b6df92e84d48f9559ad95cc6d43215528dca
sindhuja15673 <132433742+sindhuja15673@users.noreply.github.com>
2023-11-01 04:40:59 -0700

    Delete Footer.js

--- Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    return(
-    <footer className="py-1">
-    <p className="text-center text-dark mt-1">
-      @Stylish Diva, All Rights Reserved
-    </p>
-  </footer>)
-}
